a47eb57190dac0c144bd8eb8061d48116d7bba20
an1l4 <1995anila@gmail.com>
2022-09-14 14:34:41 +0530

    number-pattern

---
+++ question9.go
@@ -0,0 +1,16 @@
+//Write a program to print the number pattern
+
+package main
+
+import "fmt"
+
+func main() {
+
+	for i := 1; i <= 5; i++ {
+		for j := 1; j <= i; j++ {
+			fmt.Print(j, " ")
+		}
+		fmt.Print("\n")
+	}
+
+}

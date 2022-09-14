adf95cba00d43cfba3ac95dc3c5804d0a372a7c3
an1l4 <1995anila@gmail.com>
2022-09-14 18:35:09 +0530

    array-sorting-desc

---
+++ question12.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var size int
+	fmt.Println("enter the size of array")
+	fmt.Scanf("%d", &size)
+
+	array := make([]int, size)
+	fmt.Println("enter array elements")
+	for i := 0; i < size; i++ {
+		fmt.Scanf("%d", &array[i])
+
+	}
+	fmt.Println("array before sorting", array)
+	var temp int
+
+	for i := 0; i < size; i++ {
+		for j := i + 1; j < size; j++ {
+			if array[i] < array[j] {
+				temp = array[i]
+				array[i] = array[j]
+				array[j] = temp
+			}
+		}
+	}
+	fmt.Println("array after sorting (desc)", array)
+
+}
